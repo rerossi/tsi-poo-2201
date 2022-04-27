cdfc3747c2a7a60b1a335e83792e5107fc663821
Rodrigo Rossi <rerossi1985@gmail.com>
2022-04-26 22:07:24 -0300

    aula finalizada

--- ex UML/Config/bd.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('DSN', 'mysql:dbname=uml;host=localhost;port=3306');
-define('DB_USER', 'root');
-define('DB_PASS', '');
--- ex UML/Interfaces/Crud.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface Crud
-{
-   function criar(array $dados): bool;
-   function listar(int $id = null): array;
-   function editar(int $id, array $dados): bool;
-   function deletar(int $id): bool;
-}
